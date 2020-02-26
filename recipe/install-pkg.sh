639c27a5e49e54592b4a200b1cb281ea82fc5522
Isuru Fernando <isuruf@gmail.com>
2020-02-26 17:11:26 +0000

    Don't hardcode

--- recipe/install-pkg.sh
+++ recipe/install-pkg.sh
@@ -6,6 +6,8 @@ do
     cp "${RECIPE_DIR}/${CHANGE}-${PKG_NAME}.sh" .
     sed -i.bak "s|@CHOST@|$CHOST|g" ${CHANGE}-${PKG_NAME}.sh 
     sed -i.bak "s|@WINSDK_VERSION@|$WINSDK_VERSION|g" ${CHANGE}-${PKG_NAME}.sh 
+    sed -i.bak "s|@PREFIX@|$PREFIX|g" ${CHANGE}-${PKG_NAME}.sh
+    sed -i.bak "s|@PKG_VERSION@|$PKG_VERSION|g" ${CHANGE}-${PKG_NAME}.sh
     sed -i.bak "s|@MSVC_HEADERS_VERSION@|$MSVC_HEADERS_VERSION|g" ${CHANGE}-${PKG_NAME}.sh 
     cp ${CHANGE}-${PKG_NAME}.sh ${PREFIX}/etc/conda/${CHANGE}.d/${CHANGE}-${PKG_NAME}.sh
 done
