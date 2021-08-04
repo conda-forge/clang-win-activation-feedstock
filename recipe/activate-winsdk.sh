782f44d1597779f86b9e821b8f4dba853418b098
Kai Tietz <ktietz@anaconda.com>
2021-08-04 11:32:01 +0200

    update activation for 12.0.0

--- recipe/activate-winsdk.sh
+++ recipe/activate-winsdk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WINSDK_VERSION=10.0.17134.0
+WINSDK_VERSION=@WINSDK_VERSION@
 
 [ -z "${CI}" ] || export CONDA_BUILD_WINSDK=/tmp/cf-ci-winsdk
 
@@ -16,7 +16,7 @@ WINSDK_DIR=${CONDA_BUILD_WINSDK}/winsdk-${WINSDK_VERSION}
 if [[ ! -d "${WINSDK_DIR}" ]]; then
   mkdir -p ${WINSDK_DIR}
   pushd ${WINSDK_DIR}
-    curl -L -o win10sdk.iso https://go.microsoft.com/fwlink/p/?linkid=870809
+    curl -L -o win10sdk.iso https://go.microsoft.com/fwlink/p/?LinkID=2033686
     mkdir -p win10sdk_iso
     pushd win10sdk_iso
       7z x ../win10sdk.iso -aoa
@@ -38,40 +38,54 @@ if [[ ! -d "${WINSDK_DIR}" ]]; then
       mv "tmp/Program Files/Windows Kits/10/Include/${WINSDK_VERSION}"/* ${WINSDK_DIR}/include/
     popd
 
-    # Make symlinks for libraries
-    for f in $(find ${WINSDK_DIR}/lib/um/x64 -name "*.[L|l]ib"); do
-        name=$(basename $f)
-        full_lower=$(echo "$name" | awk '{print tolower($0)}')
-        if [[ "$name" != "$full_lower" ]]; then
-            ln -sf "$f" "$(dirname $f)/$full_lower"
-        fi
-        lib_lower="${name:0:${#name} - 4}.lib"
-        if [[ "$lib_lower" != "$name" && "$lib_lower" != "$full_lower" ]]; then
-            ln -sf "$f" "$(dirname $f)/$lib_lower"
-        fi
-    done
-    
-    # Create a VFS overlay as the Win SDK assumes case insensitive file system
-    echo "version: 0" > winsdk_vfs_overlay.yaml
-    echo "case-sensitive: false" >> winsdk_vfs_overlay.yaml
-    echo "roots:" >> winsdk_vfs_overlay.yaml
+    # Check for case sensitity of the underlying filesystem.
+    # On OSX filesystems can be either in case-sensitive or -insensitive mode,
+    # the latter is the default.
+    mkdir ${CONDA_BUILD_WINSDK}/case-sensitivity
+    touch ${CONDA_BUILD_WINSDK}/case-sensitivity/a
+    touch ${CONDA_BUILD_WINSDK}/case-sensitivity/A
+    num_files=$(find ${CONDA_BUILD_WINSDK}/case-sensitivity -type f | wc -l)
+    rm -r ${CONDA_BUILD_WINSDK}/case-sensitivity
 
-    for dir in $(find ${WINSDK_DIR}/include -type d); do
-        files=$(find $dir -maxdepth 1 -name "*.h")
-        if [[ "$files" != "" ]]; then
-            echo "  - name: \"$dir\"" >> winsdk_vfs_overlay.yaml
-            echo "    type: directory" >> winsdk_vfs_overlay.yaml
-            echo "    contents:" >> winsdk_vfs_overlay.yaml
-            for f in $files; do
-                echo "      - name: \"$(basename $f)\"" >> winsdk_vfs_overlay.yaml
-                echo "        type: file" >> winsdk_vfs_overlay.yaml
-                echo "        external-contents: \"$f\"" >> winsdk_vfs_overlay.yaml
-            done
-        fi
-    done
-    # Add symlinks for LLVMs WinMSVC.cmake
-    ln -sf ${WINSDK_DIR}/include ${WINSDK_DIR}/Include
-    ln -sf ${WINSDK_DIR}/lib ${WINSDK_DIR}/Lib
+    # file system is case sensitive
+    if [ "$num_files" -eq "2" ]; then
+      echo "Making symlinks for case-sensitive filesystems"
+      # Make symlinks for libraries
+      for f in $(find ${WINSDK_DIR}/lib/um/x64 -name "*.[L|l]ib"); do
+          name=$(basename $f)
+          full_lower=$(echo "$name" | awk '{print tolower($0)}')
+          if [[ "$name" != "$full_lower" ]]; then
+              ln -sf "$f" "$(dirname $f)/$full_lower"
+          fi
+          lib_lower="${name:0:${#name} - 4}.lib"
+          if [[ "$lib_lower" != "$name" && "$lib_lower" != "$full_lower" ]]; then
+              ln -sf "$f" "$(dirname $f)/$lib_lower"
+          fi
+      done
+
+      # Create a VFS overlay as the Win SDK assumes case insensitive file system
+      echo "version: 0" > winsdk_vfs_overlay.yaml
+      echo "case-sensitive: false" >> winsdk_vfs_overlay.yaml
+      echo "roots:" >> winsdk_vfs_overlay.yaml
+
+      for dir in $(find ${WINSDK_DIR}/include -type d); do
+          files=$(find $dir -maxdepth 1 -name "*.h")
+          if [[ "$files" != "" ]]; then
+              echo "  - name: \"$dir\"" >> winsdk_vfs_overlay.yaml
+              echo "    type: directory" >> winsdk_vfs_overlay.yaml
+              echo "    contents:" >> winsdk_vfs_overlay.yaml
+              for f in $files; do
+                  echo "      - name: \"$(basename $f)\"" >> winsdk_vfs_overlay.yaml
+                  echo "        type: file" >> winsdk_vfs_overlay.yaml
+                  echo "        external-contents: \"$f\"" >> winsdk_vfs_overlay.yaml
+              done
+          fi
+      done
+
+      # Add symlinks for LLVMs WinMSVC.cmake
+      ln -sf ${WINSDK_DIR}/include ${WINSDK_DIR}/Include
+      ln -sf ${WINSDK_DIR}/lib ${WINSDK_DIR}/Lib
+    fi
     rm -rf win10sdk_iso
     rm -rf win10sdk.iso
   popd
