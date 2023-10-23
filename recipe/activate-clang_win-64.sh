e3de2077ec64e0f56b52f773ac35371be0da8046
Bela Stoyan <bela.stoyan@gmail.com>
2023-10-23 10:10:12 +0200

    use -manifest:no as LDFLAG

--- recipe/activate-clang_win-64.sh
+++ recipe/activate-clang_win-64.sh
@@ -113,7 +113,7 @@ CPPFLAGS_USED="-D_CRT_SECURE_NO_WARNINGS -D_MT -D_DLL --target=@CHOST@ -nostdlib
 if [ -f "${CONDA_BUILD_WINSDK}/winsdk-@WINSDK_VERSION@/winsdk_vfs_overlay.yaml" ]; then
   CPPFLAGS_USED="${CPPFLAGS_USED} -Xclang -ivfsoverlay -Xclang ${CONDA_BUILD_WINSDK}/winsdk-@WINSDK_VERSION@/winsdk_vfs_overlay.yaml"
 fi
-LDFLAGS_USED="--target=@CHOST@ -nostdlib -Xclang --dependent-lib=msvcrt -fuse-ld=lld"
+LDFLAGS_USED="--target=@CHOST@ -nostdlib -Xclang --dependent-lib=msvcrt -fuse-ld=lld -manifest:no"
 LDFLAGS_USED="${LDFLAGS_USED} -Wl,-defaultlib:@PREFIX@/lib/clang/@MAJOR_VER@/lib/windows/clang_rt.builtins-x86_64.lib"
 
 _tc_activation \
