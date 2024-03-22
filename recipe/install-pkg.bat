8439258c49bf6e420520019a0692e80bb8d8896e
H. Vetinari <h.vetinari@gmx.com>
2024-03-22 17:45:07 +1100

    %ERRORLEVEL% beautifications

--- recipe/install-pkg.bat
+++ recipe/install-pkg.bat
@@ -9,12 +9,12 @@ rem We need the additional z in the activation name to have the activation call
 rem run after the vs2017 activation. See https://github.com/conda/conda/pull/7176
 rem for conda's ordering implementation.
 copy "%RECIPE_DIR%\activate-clang_win-64.bat" "%PREFIX%\etc\conda\activate.d\vs%VSYEAR%_z-clang_win-64.bat"
-if errorlevel 1 exit /B 1
+if %ERRORLEVEL% neq 0 exit 1
 pushd "%PREFIX%\etc\conda\activate.d"
 sed -i 's/@CFLAGS@/%FINAL_CFLAGS%/g' vs%VSYEAR%_z-clang_win-64.bat
-if errorlevel 1 exit /B 1
+if %ERRORLEVEL% neq 0 exit 1
 sed -i 's/@CXXFLAGS@/%FINAL_CXXFLAGS%/g' vs%VSYEAR%_z-clang_win-64.bat
-if errorlevel 1 exit /B 1
+if %ERRORLEVEL% neq 0 exit 1
 sed -i 's/@MAJOR_VER@/%MAJOR_VER%/g' vs%VSYEAR%_z-clang_win-64.bat
-if errorlevel 1 exit /B 1
+if %ERRORLEVEL% neq 0 exit 1
 popd
