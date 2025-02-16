6c322629e0c87c1cf0324cc3989105ad314bd20d
H. Vetinari <h.vetinari@gmx.com>
2025-02-16 15:20:53 +1100

    account for different default output names

--- recipe/run_test_clang.bat
+++ recipe/run_test_clang.bat
@@ -2,5 +2,11 @@
 
 %CC% %CFLAGS% test.c %LDFLAGS% -v
 if %ERRORLEVEL% neq 0 exit 1
-a.exe
-if %ERRORLEVEL% neq 0 exit 1
+if [%CC%] == [clang-cl.exe] (
+    REM clang-cl has a different default exe name
+    test.exe
+    if %ERRORLEVEL% neq 0 exit 1
+) else (
+    a.exe
+    if %ERRORLEVEL% neq 0 exit 1
+)
