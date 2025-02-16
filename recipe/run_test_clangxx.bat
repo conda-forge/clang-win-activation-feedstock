ecd81ae46510a53c444e11513386d544d7563839
H. Vetinari <h.vetinari@gmx.com>
2025-02-16 17:34:42 +1100

    also execute run_test_clangxx for clang-cl

--- recipe/run_test_clangxx.bat
+++ recipe/run_test_clangxx.bat
@@ -2,5 +2,11 @@
 
 %CXX% %CXXFLAGS% test.cpp %LDFLAGS% -v
 if %ERRORLEVEL% neq 0 exit 1
-a.exe
-if %ERRORLEVEL% neq 0 exit 1
+if [%CXX%] == [clang-cl.exe] (
+    REM clang-cl has a different default exe name
+    test.exe
+    if %ERRORLEVEL% neq 0 exit 1
+) else (
+    a.exe
+    if %ERRORLEVEL% neq 0 exit 1
+)
