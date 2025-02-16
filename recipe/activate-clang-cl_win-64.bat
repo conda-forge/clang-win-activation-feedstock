20d0245eb9dce0631c961dcd3a1b7df429d2787a
H. Vetinari <h.vetinari@gmx.com>
2025-02-16 13:58:21 +1100

    add separate output for clang-cl mode

--- recipe/activate-clang-cl_win-64.bat
+++ recipe/activate-clang-cl_win-64.bat
@@ -1,6 +1,7 @@
 @echo on
 
-set "CC=clang.exe"
+set "CC=clang-cl.exe"
+set "CXX=clang-cl.exe"
 set "NM=llvm-nm.exe"
 set "LD=lld-link.exe"
 set "AR=llvm-ar.exe"
@@ -9,3 +10,4 @@ set "RANLIB=llvm-ranlib.exe"
 set "CPPFLAGS_USED=-D_CRT_SECURE_NO_WARNINGS -nostdlib -fms-runtime-lib=dll -fuse-ld=lld -fno-aligned-allocation"
 set "LDFLAGS=-nostdlib -Wl,-defaultlib:%CONDA_PREFIX:\=/%/lib/clang/@MAJOR_VER@/lib/windows/clang_rt.builtins-x86_64.lib"
 set "CFLAGS=@CFLAGS@ %CPPFLAGS_USED%"
+set "CXXFLAGS=@CXXFLAGS@ %CPPFLAGS_USED%"
--- recipe/activate-clang-cl_win-64.ps1
+++ recipe/activate-clang-cl_win-64.ps1
@@ -1,4 +1,5 @@
 $Env:CC="clang.exe"
+$Env:CXX="clang++.exe"
 $Env:NM="llvm-nm.exe"
 $Env:LD="lld-link.exe"
 $Env:AR="llvm-ar.exe"
@@ -7,3 +8,4 @@ $Env:RANLIB="llvm-ranlib.exe"
 $Env:CPPFLAGS_USED="-D_CRT_SECURE_NO_WARNINGS -nostdlib -fms-runtime-lib=dll -fuse-ld=lld -fno-aligned-allocation"
 $Env:LDFLAGS="-nostdlib -Wl,-defaultlib:" + $Env:CONDA_PREFIX.Replace("\","/") +"/lib/clang/@MAJOR_VER@/lib/windows/clang_rt.builtins-x86_64.lib"
 $Env:CFLAGS="@CFLAGS@ " + $Env:CPPFLAGS_USED
+$Env:CXXFLAGS="@CXXFLAGS@ " + $Env:CPPFLAGS_USED
