3009d9b4f9647f02f1576f3d6d4588f7bcf6a722
H. Vetinari <h.vetinari@gmx.com>
2025-02-16 16:55:20 +1100

    move /std:c++17 to CXXFLAGS for clang-cl

--- recipe/activate-clang-cl_win-64.ps1
+++ recipe/activate-clang-cl_win-64.ps1
@@ -8,4 +8,4 @@ $Env:RANLIB="llvm-ranlib.exe"
 $Env:CPPFLAGS_USED="-D_CRT_SECURE_NO_WARNINGS -nostdlib -fms-runtime-lib=dll -fuse-ld=lld -fno-aligned-allocation"
 $Env:LDFLAGS="-nostdlib /link /DEFAULTLIB:" + $Env:CONDA_PREFIX +"\lib\clang\@MAJOR_VER@\lib\windows\clang_rt.builtins-x86_64.lib"
 $Env:CFLAGS="@CFLAGS@ " + $Env:CPPFLAGS_USED
-$Env:CXXFLAGS="@CXXFLAGS@ " + $Env:CPPFLAGS_USED
+$Env:CXXFLAGS="@CXXFLAGS@ /std:c++17 " + $Env:CPPFLAGS_USED
