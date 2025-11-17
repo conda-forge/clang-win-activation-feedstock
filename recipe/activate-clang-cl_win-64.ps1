87e9630fa217d86f6f1b7fb8df8218142f912611
Frankie Robertson <frankie@robertson.name>
2025-11-17 13:43:09 +0200

    Pass -fuse-ld=lld-link instead of -fuse-ld=lld

--- recipe/activate-clang-cl_win-64.ps1
+++ recipe/activate-clang-cl_win-64.ps1
@@ -5,7 +5,7 @@ $Env:LD="lld-link.exe"
 $Env:AR="llvm-ar.exe"
 $Env:RANLIB="llvm-ranlib.exe"
 
-$Env:CPPFLAGS_USED="-D_CRT_SECURE_NO_WARNINGS -fms-runtime-lib=dll -fuse-ld=lld"
+$Env:CPPFLAGS_USED="-D_CRT_SECURE_NO_WARNINGS -fms-runtime-lib=dll -fuse-ld=lld-link"
 $Env:LDFLAGS="/link /DEFAULTLIB:" + $Env:CONDA_PREFIX +"\lib\clang\@MAJOR_VER@\lib\windows\clang_rt.builtins-x86_64.lib"
 $Env:CFLAGS="@CFLAGS@ " + $Env:CPPFLAGS_USED
 $Env:CXXFLAGS="@CXXFLAGS@ /std:c++17 " + $Env:CPPFLAGS_USED
