4607a86b13328f905d75b4af58ceddf7ccf8c782
H. Vetinari <h.vetinari@gmx.com>
2026-01-06 16:08:38 +1100

    also replace backslashes in paths from env var

--- recipe/activate-clang-cl_win-64.bat
+++ recipe/activate-clang-cl_win-64.bat
@@ -8,6 +8,6 @@ set "AR=llvm-ar.exe"
 set "RANLIB=llvm-ranlib.exe"
 
 set "CPPFLAGS_USED=-D_CRT_SECURE_NO_WARNINGS -fms-runtime-lib=dll -fuse-ld=lld"
-set "LDFLAGS=/DEFAULTLIB:%CONDA_PREFIX%/lib/clang/@MAJOR_VER@/lib/windows/clang_rt.builtins-x86_64.lib"
+set "LDFLAGS=/DEFAULTLIB:%CONDA_PREFIX:\=/%/lib/clang/@MAJOR_VER@/lib/windows/clang_rt.builtins-x86_64.lib"
 set "CFLAGS=@CFLAGS@ %CPPFLAGS_USED%"
 set "CXXFLAGS=@CXXFLAGS@ /std:c++17 %CPPFLAGS_USED%"
