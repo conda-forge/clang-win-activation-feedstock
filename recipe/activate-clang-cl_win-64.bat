57f1981c74d914ee597b666e2f09057dd7b1d801
Lucas Colley <lucas.colley8@gmail.com>
2026-03-18 10:06:02 +0000

    make clang-cl activation compatible with flang
    Co-authored-by: Isuru Fernando <isuruf@gmail.com>

--- recipe/activate-clang-cl_win-64.bat
+++ recipe/activate-clang-cl_win-64.bat
@@ -8,6 +8,12 @@ set "AR=llvm-ar.exe"
 set "RANLIB=llvm-ranlib.exe"
 
 set "CPPFLAGS_USED=-DNDEBUG -D_CRT_SECURE_NO_WARNINGS -fms-runtime-lib=dll -fuse-ld=lld"
+
 set "LDFLAGS=/DEFAULTLIB:%CONDA_PREFIX:\=/%/lib/clang/@MAJOR_VER@/lib/windows/clang_rt.builtins-x86_64.lib"
+REM `-Xlinker` was only exposed in v20. It is needed for compatibility with the `flang` CLI
+if "@MAJOR_VER@" GEQ "20" (
+    set "LDFLAGS=-Xlinker %LDFLAGS%"
+)
+
 set "CFLAGS=@CFLAGS@ %CPPFLAGS_USED%"
 set "CXXFLAGS=@CXXFLAGS@ /std:c++17 %CPPFLAGS_USED%"
